6fc33f7113cf2b725f194cd233cf56e1be4b403d
deepakc <deepak.c@servify.in>
2019-11-06 16:14:35 +0530

    added navbar destination drop down selection

--- src/common/components/Navbar/Navbar.js
+++ src/common/components/Navbar/Navbar.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faBars, faHome, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { Drawer, List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
+import { Drawer, List, ListItem, ListItemText, ListItemIcon,
+  Select, MenuItem, FormControl } from '@material-ui/core';
+import { getQueryparams } from '../../../utils/functions'
 import './Navbar.scss';
 
 class Navbar extends Component {
     state = {
-        isDrawerOpen: false
+        isDrawerOpen: false,
+        selectedDestination: getQueryparams(this.props.location.search).country
+    }
+
+    componentWillReceiveProps (nextProps) {
+      if (this.props !== nextProps) {
+        this.setState({
+          selectedDestination: getQueryparams(nextProps.location.search).country
+        })
+      }
     }
 
     listOption = [{
@@ -50,7 +61,17 @@ class Navbar extends Component {
         );
     }
 
+    // tour?country=uganda
+    handleDestinationSelection = (event) => {
+      this.setState({
+        selectedDestination: event.target.value
+      }, () => {
+        this.props.history.push('/tour?country=' + this.state.selectedDestination.toLowerCase())
+      })
+    }
+
     render() {
+      console.log('props: ', this.props)
         return (
             <nav className="desktop-navigation">
                 <ul>
@@ -59,6 +80,20 @@ class Navbar extends Component {
                     </li>
                     <li>
                         <Link to="/contact-us">Contact Us</Link>
+                    </li>
+                    <li>
+                      <FormControl>
+                        {!this.state.selectedDestination && <span id="drop-down-label">Destinations</span>}
+                        <Select
+                          labelId="drop-down-label"
+                          id="demo-simple-select"
+                          value={this.state.selectedDestination}
+                          onChange={this.handleDestinationSelection}>
+                          <MenuItem value='kenya'>Kenya</MenuItem>
+                          <MenuItem value='uganda'>Uganda</MenuItem>
+                          <MenuItem value='tanzania'>Tanzania</MenuItem>
+                        </Select>
+                      </FormControl>
                     </li>
                 </ul>
                 <div className="mobile-navigation">
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,18 @@
 export const getElementProps = (ele) => {
   return ele.getBoundingClientRect()
 }
+
+
+export const getQueryparams = (qry) => {
+  let params = qry.replace('?', '')
+  params = params.replace('?', '').split('&') // [step=1, step=2]
+  let queryObj = null
+  params.map((data, index) => {
+    let dummy = data.split('=')
+    queryObj = {
+      ...queryObj,
+      [dummy[0]]: dummy[1]
+    }
+  })
+  return queryObj
+}
