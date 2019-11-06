f5ed3ca70430f091bdde12406077eec3e53df0d2
deepakc <deepak.c@servify.in>
2019-11-06 20:08:37 +0530

    linting fixes

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,8 +3,7 @@ export const getElementProps = (ele) => {
 }
 
 
-export const getQueryparams = (qry) => {
-  let params = qry.replace('?', '')
+export const getQueryparams = (params) => {
   params = params.replace('?', '').split('&') // [step=1, step=2]
   let queryObj = null
   params.map((data, index) => {
@@ -13,6 +12,7 @@ export const getQueryparams = (qry) => {
       ...queryObj,
       [dummy[0]]: dummy[1]
     }
+    return queryObj
   })
   return queryObj
 }
